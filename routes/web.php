f49978ff650a7b64784f3523fbd5eaf193fb8e87
petersudario <pedrohesudario@hotmail.com>
2024-07-11 20:59:25 -0300

    Removendo rota profile

--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,9 @@ use App\Http\Controllers\MapController;
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [MapController::class, 'index'])->name('profile.edit');
     Route::get('/', [MapController::class, 'index'])->name('home');
-
     Route::get('/unidades/total-sales', [MapController::class, 'getTotalSales']);
-
     Route::get('/vendas', [VendasController::class, 'index'])->name('vendas.index');
-
-
     Route::get('/unidades/{currentUnidadeId}/nearby', [VendasController::class, 'searchNearbyUnidades']);
     
 });
